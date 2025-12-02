a18de112edce8fd3763bbc5f5a7b7307aeb171c7
felipe cardenas <felipecas097@gmail.com>
2025-12-01 20:28:27 -0500

    [+] user.model : defube estructura de datos [+] users.route.js: Obtiene, Registra usuario, Responde al cliente

---
+++ src/models/User.models.js
@@ -0,0 +1,53 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    username: {
+        type: String,           //Define el tipo
+        required: true,         //Es obligatorio
+        // modificador
+        unique: true,           // Obliga a que el valor sea unico
+        trim: true,             // Elimina los espacios en blanco (inicio/final del string)
+        lowercase: true         // Transforma todo a minusculas 
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true, 
+        trim: true,
+        minLenght: 8,
+        maxLenght: 12,
+    },
+    role: {
+        type: String, 
+        required: true,
+        enum: ['super-admin','admin', 'colaborator', 'editor', 'registered'],
+        default: 'registered',
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    // isVerified: {
+    //     code: String,
+    //     trim: true
+    // }
+},{});
+// Crear el modelo User basado en el esquema userSchema 
+const userModel = model (
+    'users',                        // Nombre de la coleccion en singular 'User'
+     userSchema                      //Esquema asociado al modelo 
+);
+
+// Exportando el modelo User, paraque sea usado en otras partes de la aplicacion
+export default userModel; 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,33 +1,23 @@
 import express from 'express' 
+import userModel from '../models/User.models.js';
 
 const router = express.Router();
 
-// router.get( '/', ( req, res ) => {
-//     res.json({ msg: 'Obtiene todos los usuarios' });
-// } );
 
-// router.post( '/', ( req, res ) => {
-//     res.json({ msg: 'Crear un usuario' });
-// } );
 
-// router.put( '/', ( req, res ) => {
-//     res.json({ msg: 'Actualiza todos las propiedades del usuario' });
-// } );
-
-// router.patch( '/', ( req, res ) => {
-//     res.json({ msg: 'Actualiza parcialmente 1 o todas las propiedades del usuario' });
-// } );
-
-// router.delete( '/', ( req, res ) => {
-//     res.json({ msg: 'Elimina un usuario' });
-// } );
-
-router.post( '/',( req, res ) => {
+router.post( '/', async ( req, res ) => {
     const data = req.body;
 
     console.log( data);
-    res.json({ msg:'create users', data
-    })
-})
+
+    //Registrar los datos usando el userModel
+   const dataRegistered = await userModel.create( data );   //Registrar los datos en la base de datos
+
+    // Responder al cliente
+    res.json({ msg:'create users',
+         //data: data,             // Forma tradicional
+         dataRegistered            // ECMAScript 2015 
+    });
+});
 
 export default router;
